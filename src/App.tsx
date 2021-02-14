7506258bc7f11b189e8ab8520898261e819b7043
Anton <anton.bartcev@gmail.com>
2021-02-15 01:07:53 +0200

    New feature: when star on card is clicked, card added or removed from initial screen when no search performed

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,156 @@
 import "./styles/App.css";
 import React from "react";
-import { List } from "immutable"
+import { List, Set } from "immutable"
 import CharacterInfo from "../src/components/CharacterInfo"
 import "./styles/Components.css"
 import warningImg from "../src/components/utils"
 import { ResponseData } from "../src/components/CharacterInfo"
+import { debounce } from "lodash"
+
 
 
 type AppState = {
+  // Input field value
   input: string,
+  // List of characters found by name
+  // null - no search performed
   response: null | List<ResponseData>,
-  bIsPendingResponse: boolean
+  // true - pending response started
+  // false - pending response stopped
+  bIsPendingResponse: boolean,
+  // Set of names of cards with activated star
+  setOfActivatedCards: Set<string[]>,
+  // list of data of cards with activated star
+  listOfActivatedCards: List<ResponseData>
 }
 
 
 // Main app structure
 class App extends React.Component<{}, AppState> {
+  private debouncedFetchName: Function
+
   constructor(props: AppState) {
     super(props);
     this.state = {
-      // Input field value
       input: "",
-      // List of characters found by name
-      // null - no search performed
       response: null,
-      // true - pending response started
-      // false - pending response stopped
-      bIsPendingResponse: false
+      bIsPendingResponse: false,
+      setOfActivatedCards: Set(),
+      listOfActivatedCards: List()
     }
     this.onInputChange = this.onInputChange.bind(this);
-    this.fetchName = this.fetchName.bind(this)
-    this.getSearchOutput = this.getSearchOutput.bind(this)
+    this.getSearchOutput = this.getSearchOutput.bind(this);
+    this.debouncedFetchName = debounce(this.fetchName, 500);
+    this.setCardAction = this.setCardAction.bind(this)
   }
-  onInputChange(event: React.FormEvent<HTMLInputElement>) {
+
+  // Either delete or add card when star is clicked
+  setCardAction(bIsNameContained: boolean, name: string[], id: number) {
+    const { setOfActivatedCards, response, listOfActivatedCards } = this.state;
+    const responseAsList = response as List<ResponseData>
+    const responseData = responseAsList.get(id) as ResponseData
+    // If name is contained in setOfActivatedCards, delete card's name and data
+    if (bIsNameContained) {
+      this.setState({
+        ...this.state,
+        // Delete name from the set
+        setOfActivatedCards: setOfActivatedCards.delete(name),
+        // Delete data by id from the list
+        listOfActivatedCards: listOfActivatedCards.delete(id)
+      })
+    }
+    // If name is not contained on setOfActivatedCards, add card's name and data
+    else {
+      this.setState({
+        ...this.state,
+        // Add name to set
+        setOfActivatedCards: setOfActivatedCards.add(name),
+        // Add data to list
+        listOfActivatedCards: listOfActivatedCards.concat(responseData)
+      })
+    }
+  }
+
+  onInputChange({ currentTarget: { value } }: React.FormEvent<HTMLInputElement>) {
     // Input of user
-    const text = event.currentTarget.value;
+    this.debouncedFetchName(value);
     // Update input field value to present new input 
     this.setState({
       ...this.state,
-      input: text
+      input: value,
+      bIsPendingResponse: true
     })
   }
-  fetchName(event: React.KeyboardEvent<object>) {
-    // Get input of user        
-    const name = (event.currentTarget as HTMLInputElement).value;
-    // Check if user pressed enter
-    if (event.key === "Enter") {
-      this.setState({
-        ...this.state,
-        bIsPendingResponse: true
+
+  fetchName(name: string) {
+    if (name.length < 1)
+      return;
+    // Find character by input of user from API
+    fetch("https://swapi.dev/api/people/?search=" + name)
+      .then(Response => {
+        return Response.json()
       })
-      // Find character by input of user from API
-      fetch("https://swapi.dev/api/people/?search=" + name)
-        .then(Response => {
-          return Response.json()
-        })
-        .then(jsonData =>
-          // Update state with found characters
-          this.setState({
-            ...this.state,
-            response: List(jsonData.results),
-            bIsPendingResponse: false
-          }))
-    }
+      .then(jsonData =>
+        // Update state with found characters
+        this.setState({
+          ...this.state,
+          response: List(jsonData.results),
+          bIsPendingResponse: false
+        }))
+
   }
-  // Decide what message will be shown to user 
+
+  // Decide what message will be shown to user
   // after and while performing the search
   getSearchOutput() {
-    const { bIsPendingResponse, response } = this.state
+    const { bIsPendingResponse, response, input, setOfActivatedCards, listOfActivatedCards } = this.state
     const responseAsList = response as List<ResponseData>
     // Loading animation while pending response
-    if (bIsPendingResponse) {
+    if (bIsPendingResponse && input.length > 0) {
       return <div className="searchOutputContainer">
         <div className="ldsDualRing"></div>
       </div>
     }
     // Initial state
-    if (response === null) {
+    if ((response === null || input.length === 0) && listOfActivatedCards.size === 0) {
       return <div className="searchOutputContainer">
-        <p>type name of character and press Enter</p>
+        <p>type name of Star Wars character</p>
       </div>
     }
     // If character is not found by name
-    else if (responseAsList.size === 0) {
+    else if (responseAsList.size === 0 && input.length > 0) {
       return <div className="searchOutputContainer">
         {warningImg}
         <p className="warning">nothing found</p>
       </div>
     }
     // If character is found by name
-    else {
+    else if (responseAsList.size > 0 && input.length > 0) {
       return responseAsList
         .map((info: ResponseData, key: number) =>
           <CharacterInfo
             key={key}
             info={info}
+            setStarAction={this.setCardAction}
+            setOfActivatedCards={setOfActivatedCards}
+            // Id of card in responseAsList
+            id={responseAsList.indexOf(info)}
           />)
     }
+    // If any card has activated star
+    else if (input.length === 0 && listOfActivatedCards.size > 0) {
+      return listOfActivatedCards.map((info: ResponseData, key: number) =>
+        <CharacterInfo
+          key={key}
+          info={info}
+          setStarAction={this.setCardAction}
+          setOfActivatedCards={setOfActivatedCards}
+          // Id of card in listOfActivatedCards 
+          id={listOfActivatedCards.indexOf(info)}
+        />)
+    }
   }
+
   render() {
     return <div className="App">
       <h1>Star Wars Characters</h1>
@@ -105,7 +158,7 @@ class App extends React.Component<{}, AppState> {
         value={this.state.input}
         onChange={this.onInputChange}
         placeholder={"type name of character..."}
-        onKeyDown={this.fetchName} />
+      />
       {this.getSearchOutput()}
     </div>
   }
--- src/components/CharacterInfo.tsx
+++ src/components/CharacterInfo.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import "../styles/Components.css"
 
 
 
+// Type of card's data
 export interface ResponseData {
     name: string,
     skin_color: string,
@@ -11,58 +11,83 @@ export interface ResponseData {
     films: Array<string>
 }
 
+// Types of props passed from App.tsx
 type InfoProps = {
-    info: ResponseData
+    info: ResponseData,
+    setStarAction: (bIsNameContained: boolean, name: string[], id: number) => void
+    setOfActivatedCards: import("c:/Coding/React coding/api_type_script/node_modules/immutable/dist/immutable-nonambient").Set<string[]>,
+    id: number
 }
 
-// Card with information about found character
-class CharacterInfo extends React.Component<InfoProps> {
+const CharacterInfo = (props: InfoProps) => {
+    const { info, setOfActivatedCards, id, setStarAction } = props
+
+    // Add or remove card from initial page by clicking on star 
+    const toggleStar = () => {
+        const nameOfCharAsStrArr = nameOfCharacter[0] as string[]
+        setStarAction(setOfActivatedCards.contains(nameOfCharAsStrArr), nameOfCharAsStrArr, id)
+    }
+
     // Format information fetched from API to present it in the app
-    formatInfo( { name, skin_color, gender, mass, films }: ResponseData ) {
+    const formatInfo = ({ name, skin_color, gender, mass, films }: ResponseData) => {
         // Skin color of character
         const skinColorInfo = "Skin color: " + skin_color + ", "
         // Gender of character
         const genderInfo = "Gender: " + gender + ", "
         // Mass of character (if mass is unknown, no need to add "kg" clarification)
         const massInfo = "Mass: " + mass +
-            ( mass === "unknown" ? "" : "kg" )
+            (mass === "unknown" ? "" : "kg")
         // Array of films character participated in
         const filmsOutputArr = []
-        for ( const film of films ) {
+        for (const film of films) {
             // Film as string
-            const output = ( films.indexOf( film ) + 1 ) + ": " + film
-            filmsOutputArr.push( output )
+            const output = (films.indexOf(film) + 1) + ": " + film
+            filmsOutputArr.push(output)
         }
         return [name, skinColorInfo, genderInfo, massInfo, filmsOutputArr]
     }
-    render() {
-        const { info } = this.props
-        // Array of information about characters
-        const arrayOfInfoPoints = this.formatInfo( info )
-        // Array of films 
-        const arrayOfFilms = arrayOfInfoPoints[arrayOfInfoPoints.length - 1] as Array<string>
-        // Remove array of films from main info array 
-        // to put separately on page
-        arrayOfInfoPoints.splice( arrayOfInfoPoints.length - 1, 1 )
-        const nameOfCharecter = arrayOfInfoPoints.splice( 0, 1 )
-        return <div className="characterInfoParentDiv">
-            <h2 className="name">{nameOfCharecter} </h2>
-            {arrayOfInfoPoints.map( ( el, key ) =>
-                <div
-                    key={key}
-                    className="info">
+
+    const arrayOfInfoPoints = formatInfo(info)
+
+    // Array of films 
+    const arrayOfFilms = arrayOfInfoPoints[arrayOfInfoPoints.length - 1] as Array<string>
+
+    // Remove array of films from main info array 
+    // to put separately on page
+    arrayOfInfoPoints.splice(arrayOfInfoPoints.length - 1, 1)
+
+    // Name of card
+    const nameOfCharacter = arrayOfInfoPoints.splice(0, 1)
+
+    // Change class name of star depending on 
+    // existence of card's name in the setOfActivatedCards
+    const starClass =
+        props.setOfActivatedCards.contains(nameOfCharacter[0] as string[])
+            ?
+            "zmdi zmdi-star zmdi-hc-3x"
+            :
+            "zmdi zmdi-star-outline zmdi-hc-3x"
+
+    return (<div className="characterInfoParentDiv">
+        <h2 className="name">{nameOfCharacter} </h2>
+        <div className="star" onMouseDown={toggleStar}>
+            <i className={starClass}></i>
+        </div>
+        {arrayOfInfoPoints.map((el, key) =>
+            <div
+                key={key}
+                className="info">
+                {el}
+            </div>)
+        }
+        <h2 className="films">Films: </h2>
+        {
+            arrayOfFilms.map((el: string, key: number) =>
+                <div className="info"
+                    key={key}>
                     {el}
-                </div> )
-            }
-            <h2 className="films">Films: </h2>
-            {
-                arrayOfFilms.map( ( el: string, key: number ) =>
-                    <div className="info"
-                        key={key}>
-                        {el}
-                    </div> )
-            }
-        </div >
-    }
+                </div>)
+        }
+    </div >);
 }
 export default CharacterInfo;
